bb265ad99808850a3c0acbb7d9ea3ed685da6c23
bitbytejoy <bitbytejoy@gmail.com>
2023-07-26 11:47:19 +0200

    BIG BANG

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+type KomponentProps = {
+  name: string;
+  dateOfBirth?: string;
+};
+
+function Komponent(props: KomponentProps) {
+  return <h1>Hello {props.name} {props.dateOfBirth}</h1>;
+}
+
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+      <div>
+        <Komponent name={"John"}/>
+        <Komponent name={"Jane"} dateOfBirth={"01.01.1996"}/>
+        <Komponent name={"Blake"} dateOfBirth={"01.06.1995"}/>
+      </div>
+  );
 }
 
 export default App
